b6d85bccff9f8b149b11e2ad888f06f4253e6e6b
Will Raxworthy <me@willrax.com>
2017-04-08 09:41:32 +0100

    create city report and weather fetcher

--- src/ui/components/weather-app/component.ts
+++ src/ui/components/weather-app/component.ts
@@ -1,5 +1,55 @@
-import Component from "@glimmer/component";
+import Component, { tracked } from "@glimmer/component";
+
+enum LocationStatus {
+  Pending = 1,
+  Failed = 2,
+  Success = 3
+};
+
+const ApiHost = 'http://localhost:4000';
 
 export default class Weather extends Component {
+  @tracked locationStatus = LocationStatus.Pending;
+  @tracked report: any;
 
+  constructor(options) {
+    super(options);
+    this.loadWeather();
+  }
+
+  async loadWeather() {
+    try {
+      let location = await this.getLocation()
+      let response = await fetch(`${ApiHost}/${location}?units=auto`);
+      this.report = await response.json();
+      this.locationStatus = LocationStatus.Success;
+    } catch(e) {
+      this.locationStatus = LocationStatus.Failed;
+    }
+  }
+
+  @tracked('locationStatus')
+  get locationPending() {
+    return this.locationStatus === LocationStatus.Pending;
+  }
+
+  @tracked('locationStatus')
+  get locationFailed() {
+    return this.locationStatus === LocationStatus.Failed;
+  }
+
+  @tracked('locationStatus')
+  get locationSuccess() {
+    return this.locationStatus === LocationStatus.Success;
+  }
+
+  getLocation(): Promise<[number, number]> {
+    return new Promise (function(resolve, reject) {
+      navigator.geolocation.getCurrentPosition(function({ coords }) {
+        resolve([coords.latitude, coords.longitude]);
+      }, function() {
+        reject();
+      });
+    })
+  }
 }
